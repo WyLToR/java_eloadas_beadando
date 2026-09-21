eacbf82ff228e771f03d217ff04a63cfc0afb739
Norbert <norbert1984@gmail.com>
2026-09-21 23:54:56 +0200

    feat: add MNB exchange rate form

--- src/main/java/hu/nje/beadando_eloadas/BeadandoEloadasApplication.java
+++ src/main/java/hu/nje/beadando_eloadas/BeadandoEloadasApplication.java
@@ -13,6 +13,10 @@ import soapclient.MNBArfolyamServiceSoapGetExchangeRatesStringFaultFaultMessage;
 import soapclient.MNBArfolyamServiceSoapGetInfoStringFaultFaultMessage;
 import soapclient.MNBArfolyamServiceSoapImpl;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 @SpringBootApplication
 @Controller
 public class BeadandoEloadasApplication {
@@ -47,4 +51,36 @@ public class BeadandoEloadasApplication {
                 "EUR"
         );
     }
-}
+    @GetMapping("/exercise")
+    public String soapForm(Model model) {
+        model.addAttribute("param", new MessagePrice());
+        return "forms";
+    }
+
+    @PostMapping("/exercise")
+    public String soapResult(
+            @ModelAttribute MessagePrice messagePrice,
+            Model model
+    ) throws MNBArfolyamServiceSoapGetExchangeRatesStringFaultFaultMessage {
+
+        MNBArfolyamServiceSoapImpl impl =
+                new MNBArfolyamServiceSoapImpl();
+
+        MNBArfolyamServiceSoap service =
+                impl.getCustomBindingMNBArfolyamServiceSoap();
+
+        String strOut =
+                "Currency:" + messagePrice.getCurrency() + ";" +
+                        "Start date:" + messagePrice.getStartDate() + ";" +
+                        "End date:" + messagePrice.getEndDate() + ";";
+
+        strOut += service.getExchangeRates(
+                messagePrice.getStartDate(),
+                messagePrice.getEndDate(),
+                messagePrice.getCurrency()
+        );
+
+        model.addAttribute("sendOut", strOut);
+
+        return "result";
+    }}
