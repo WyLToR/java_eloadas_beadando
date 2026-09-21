82aef0f031009b4d0eea60c7875ae6ece22621f0
WyLToR <bekesi.patrik@gmail.com>
2026-09-21 17:10:31 +0200

    feat: add responsive application homepage

--- src/main/java/hu/nje/beadando_eloadas/BeadandoEloadasApplication.java
+++ src/main/java/hu/nje/beadando_eloadas/BeadandoEloadasApplication.java
@@ -2,12 +2,19 @@ package hu.nje.beadando_eloadas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class BeadandoEloadasApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BeadandoEloadasApplication.class, args);
     }
 
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
