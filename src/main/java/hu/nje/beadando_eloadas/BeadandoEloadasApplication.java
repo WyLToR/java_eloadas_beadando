11533b1ded76142a9d81ce9bc0ce8e1b1282efc4
Norbert <norbert1984@gmail.com>
2026-09-21 23:43:55 +0200

    feat: add MNB SOAP service test

--- src/main/java/hu/nje/beadando_eloadas/BeadandoEloadasApplication.java
+++ src/main/java/hu/nje/beadando_eloadas/BeadandoEloadasApplication.java
@@ -5,6 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import soapclient.MNBArfolyamServiceSoap;
+import soapclient.MNBArfolyamServiceSoapGetCurrentExchangeRatesStringFaultFaultMessage;
+import soapclient.MNBArfolyamServiceSoapGetExchangeRatesStringFaultFaultMessage;
+import soapclient.MNBArfolyamServiceSoapGetInfoStringFaultFaultMessage;
+import soapclient.MNBArfolyamServiceSoapImpl;
+
 @SpringBootApplication
 @Controller
 public class BeadandoEloadasApplication {
@@ -16,5 +24,27 @@ public class BeadandoEloadasApplication {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/feladat1")
+    @ResponseBody
+    public String soapTeszt()
+            throws MNBArfolyamServiceSoapGetInfoStringFaultFaultMessage,
+            MNBArfolyamServiceSoapGetCurrentExchangeRatesStringFaultFaultMessage,
+            MNBArfolyamServiceSoapGetExchangeRatesStringFaultFaultMessage {
+
+        MNBArfolyamServiceSoapImpl impl = new MNBArfolyamServiceSoapImpl();
+        MNBArfolyamServiceSoap service =
+                impl.getCustomBindingMNBArfolyamServiceSoap();
+
+        return service.getInfo()
+                + " "
+                + service.getCurrentExchangeRates()
+                + " "
+                + service.getExchangeRates(
+                "2022-08-14",
+                "2022-09-14",
+                "EUR"
+        );
     }
 }
