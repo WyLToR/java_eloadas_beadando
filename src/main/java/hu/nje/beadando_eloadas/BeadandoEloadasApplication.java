3bd908e956b1bd45a88670620650f24428329fb9
WyLToR <bekesi.patrik@gmail.com>
2026-09-21 17:04:22 +0200

    init project

---
+++ src/main/java/hu/nje/beadando_eloadas/BeadandoEloadasApplication.java
@@ -0,0 +1,13 @@
+package hu.nje.beadando_eloadas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeadandoEloadasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeadandoEloadasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/nje/beadando_eloadas/BeadandoEloadasApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nje.beadando_eloadas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeadandoEloadasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
