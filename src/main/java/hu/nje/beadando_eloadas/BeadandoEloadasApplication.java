6730d29e66d9ecf707faeec32c3584a54ee04847
Norbert <norbert1984@gmail.com>
2026-09-22 12:41:39 +0200

    "feat: add MNB exchange rate chart"

--- src/main/java/hu/nje/beadando_eloadas/BeadandoEloadasApplication.java
+++ src/main/java/hu/nje/beadando_eloadas/BeadandoEloadasApplication.java
@@ -17,6 +17,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.io.StringReader;import java.util.ArrayList;import java.util.List;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;import org.w3c.dom.Element;import org.w3c.dom.NodeList;import org.xml.sax.InputSource;
+
 @SpringBootApplication
 @Controller
 public class BeadandoEloadasApplication {
@@ -57,30 +61,57 @@ public class BeadandoEloadasApplication {
         return "forms";
     }
 
-    @PostMapping("/exercise")
-    public String soapResult(
+    @PostMapping("/exercise")public String soapResult(
             @ModelAttribute MessagePrice messagePrice,
-            Model model
-    ) throws MNBArfolyamServiceSoapGetExchangeRatesStringFaultFaultMessage {
+            Model model) throws Exception {
 
-        MNBArfolyamServiceSoapImpl impl =
-                new MNBArfolyamServiceSoapImpl();
+        MNBArfolyamServiceSoapImpl impl =new MNBArfolyamServiceSoapImpl();
 
-        MNBArfolyamServiceSoap service =
-                impl.getCustomBindingMNBArfolyamServiceSoap();
+        MNBArfolyamServiceSoap service =impl.getCustomBindingMNBArfolyamServiceSoap();
 
-        String strOut =
-                "Currency:" + messagePrice.getCurrency() + ";" +
-                        "Start date:" + messagePrice.getStartDate() + ";" +
-                        "End date:" + messagePrice.getEndDate() + ";";
-
-        strOut += service.getExchangeRates(
+        String result = service.getExchangeRates(
                 messagePrice.getStartDate(),
                 messagePrice.getEndDate(),
                 messagePrice.getCurrency()
         );
 
-        model.addAttribute("sendOut", strOut);
+        List<String> dates = new ArrayList<>();
+        List<Double> rates = new ArrayList<>();
+
+        DocumentBuilderFactory factory =DocumentBuilderFactory.newInstance();
+
+        Document document = factory.newDocumentBuilder()
+                .parse(new InputSource(new StringReader(result)));
+
+        NodeList days = document.getElementsByTagName("Day");
+
+        for (int i = 0; i < days.getLength(); i++) {
+
+            Element day = (Element) days.item(i);
+
+            String date = day.getAttribute("date");
+
+            NodeList rateNodes = day.getElementsByTagName("Rate");
+
+            if (rateNodes.getLength() > 0) {
+
+                Element rate = (Element) rateNodes.item(0);
+
+                String value = rate.getTextContent()
+                        .replace(",", ".");
+
+                dates.add(date);
+                rates.add(Double.parseDouble(value));
+            }
+        }
+
+        model.addAttribute("currency", messagePrice.getCurrency());
+        model.addAttribute("startDate", messagePrice.getStartDate());
+        model.addAttribute("endDate", messagePrice.getEndDate());
+
+        model.addAttribute("dates", dates);
+        model.addAttribute("rates", rates);
 
         return "result";
-    }}
+    }
+}
